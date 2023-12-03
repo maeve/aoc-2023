c2abbb6811ba582b0e0b446cb102c555471f9b61
Maeve Revels <maeve@aha.io>
2023-12-02 20:09:26 -0800

    Correct answer for day 1 part 2
    wtf oneight

--- 01/part2.rb
+++ 01/part2.rb
@@ -1,35 +1,39 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'strscan'
-
-input = File.readlines('./test-input2.txt').map(&:chomp)
+input = File.readlines('./input.txt').map(&:chomp)
 
 DIGIT_REGEX = /(one|two|three|four|five|six|seven|eight|nine|zero|[0-9])/
 
 calibration_values = input.map do |line|
-  digits = []
-  scanner = StringScanner.new(line)
+  puts "Line: #{line}"
 
-  while (digit = scanner.scan_until(DIGIT_REGEX))
-    digit = digit.match(DIGIT_REGEX)[0]
+  # Take advantage of the fact that * is a greedy operator
+  first_digit = line.match(/(one|two|three|four|five|six|seven|eight|nine|zero|[0-9])(.*)/)[1]
+  last_digit = line.match(/(.*)(one|two|three|four|five|six|seven|eight|nine|zero|[0-9])/)[2]
 
-    digits << case digit
-              when 'one' then 1
-              when 'two' then 2
-              when 'three' then 3
-              when 'four' then 4
-              when 'five' then 5
-              when 'six' then 6
-              when 'seven' then 7
-              when 'eight' then 8
-              when 'nine' then 9
-              when 'zero' then 0
-              else digit.to_i
-              end
+  digits = [first_digit, last_digit].map do |digit|
+    puts "Digit: #{digit}"
+
+    case digit
+    when 'one' then '1'
+    when 'two' then '2'
+    when 'three' then '3'
+    when 'four' then '4'
+    when 'five' then '5'
+    when 'six' then '6'
+    when 'seven' then '7'
+    when 'eight' then '8'
+    when 'nine' then '9'
+    when 'zero' then '0'
+    else digit
+    end
   end
 
+  puts "Digits: #{digits.inspect}"
+  puts "\n"
   "#{digits[0]}#{digits[-1]}".to_i
 end
 
+puts "Calibration values: #{calibration_values.inspect}"
 puts "Answer: #{calibration_values.sum}"
