3cf5f5c5070270093d1859f02636d579bd760813
Maeve Revels <maeve@aha.io>
2023-12-05 16:13:38 -0800

    Day 5 Part 1 performant

--- 05/part1.rb
+++ 05/part1.rb
@@ -7,43 +7,77 @@ class Seed
   attr_accessor :seed, :soil, :fertilizer, :water, :light, :temperature, :humidity, :location
 
   def initialize(seed)
-    self.seed = seed
+    self.seed = seed.to_i
   end
 
   def to_s
     "Seed #{seed}, soil #{soil}, fertilizer #{fertilizer}, water #{water}, light #{light}, temperature #{temperature}, humidity #{humidity}, location #{location}"
   end
+
+  def self.parse_seeds(seed_line)
+    seed_line.gsub(/seeds: /, '').split(' ').map { |seed| Seed.new(seed) }
+  end
 end
 
-seed_line = input.shift
-seeds = seed_line.gsub(/seeds: /, '').split(' ').compact.map { |seed| Seed.new(seed.to_i) }
+class Map
+  attr_accessor :source, :destination, :range_mappings
+
+  def initialize(source, destination)
+    self.source = source
+    self.destination = destination
+    self.range_mappings = []
+  end
+
+  def add_range_mapping(range_line)
+    destination_start, source_start, range_length = range_line.split(' ').map(&:to_i)
+    range_mappings << RangeMapping.new(
+      source_start:,
+      destination_start:,
+      range_length:
+    )
+  end
+
+  def process_seed(seed)
+    mapping = range_mappings.find { |range_mapping| range_mapping.has_source?(seed.send(source)) }
+
+    if mapping
+      seed.send("#{destination}=", mapping.destination(seed.send(source)))
+    else
+      seed.send("#{destination}=", seed.send(source))
+    end
+  end
+
+  def self.parse(map_header)
+    match = map_header.match(/(?<source>[a-z]+)-to-(?<destination>[a-z]+) map:/)
+    new(match[:source], match[:destination])
+  end
+
+  class RangeMapping
+    attr_reader :source_start, :destination_start, :range_length
+
+    def initialize(source_start:, destination_start:, range_length:)
+      @source_start = source_start
+      @destination_start = destination_start
+      @range_length = range_length
+    end
+
+    def has_source?(source)
+      source_start <= source && source < source_start + range_length
+    end
+
+    def destination(source)
+      destination_start + (source - source_start)
+    end
+  end
+end
+
+seeds = Seed.parse_seeds(input.shift)
 
 until input.empty?
   input.shift
-  match = input.shift.match(/(?<source>[a-z]+)-to-(?<destination>[a-z]+) map:/)
-  source_name = match[:source]
-  destination_name = match[:destination]
-
-  source_seeds = seeds.group_by(&source_name.to_sym)
-
-  until input.first.to_s.strip.empty?
-    destination_start, source_start, range_length = input.shift.split(' ').map(&:to_i)
-
-    source_values = Range.new(source_start, source_start + range_length, true).to_a
-    destination_values = Range.new(destination_start, destination_start + range_length, true).to_a
-
-    value_map = source_values.zip(destination_values).to_h
-
-    value_map.each_key do |key|
-      source_seeds[key]&.each do |seed|
-        seed.send("#{destination_name}=", value_map[key])
-      end
-    end
-
-    seeds.each do |seed|
-      seed.send("#{destination_name}=", seed.send(source_name)) unless seed.send(destination_name)
-    end
-  end
+  map = Map.parse(input.shift)
+  map.add_range_mapping(input.shift) until input.first.to_s.strip.empty?
+  seeds.each { |seed| map.process_seed(seed) }
 end
 
 puts "Seeds: \n#{seeds.map(&:to_s).join("\n")}"
