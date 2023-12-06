c36908522befafa502733c0fb638c8dcf0415e96
Maeve Revels <maeve@aha.io>
2023-12-06 01:06:58 -0800

    Day 5 Part 2 correct for test but not real input

--- 05/part2.rb
+++ 05/part2.rb
@@ -1,23 +1,41 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-input = File.readlines('./test-input.txt').map(&:chomp)
+require "active_support"
+require "active_support/core_ext"
 
-class Seed
+input = File.readlines('./input.txt').map(&:chomp)
+
+class SeedRange
   attr_accessor :seed, :soil, :fertilizer, :water, :light, :temperature, :humidity, :location
 
-  def initialize(seed)
-    self.seed = seed.to_i
+  def initialize(start, length)
+    self.seed = [(start.to_i..(start.to_i + length.to_i - 1))]
+    self.soil = []
+    self.fertilizer = []
+    self.water = []
+    self.light = []
+    self.temperature = []
+    self.humidity = []
+    self.location = []
+  end
+
+  def min_location
+    location.map { |l| l.first }.min
   end
 
   def to_s
     "Seed #{seed}, soil #{soil}, fertilizer #{fertilizer}, water #{water}, light #{light}, temperature #{temperature}, humidity #{humidity}, location #{location}"
   end
 
+  def add_attribute(name, value)
+    send(name) << value
+  end
+
   def self.parse_seeds(seed_line)
     seeds = []
-    seed_line.gsub(/seeds: /, '').split(' ').map(&:to_i).each_slice(2) do |start, range_length|
-      seeds += (start..(start + range_length - 1)).map { |seed| Seed.new(seed) }
+    seed_line.gsub(/seeds: /, '').split(' ').each_slice(2) do |start, range_length|
+      seeds << new(start, range_length)
     end
     seeds
   end
@@ -42,13 +60,24 @@ class Map
   end
 
   def process_seed(seed)
-    mapping = range_mappings.find { |range_mapping| range_mapping.has_source?(seed.send(source)) }
+    sources = seed.send(source)
 
-    if mapping
-      seed.send("#{destination}=", mapping.destination(seed.send(source)))
-    else
-      seed.send("#{destination}=", seed.send(source))
+    sources.each do |source|
+      mapping = range_mappings.find { |mapping| mapping.includes_source?(source) }
+
+      if mapping
+        seed.add_attribute(destination, mapping.destination(source))
+        mapping.unmapped_destinations(source).each do |unmapped_destination|
+          seed.add_attribute(destination, unmapped_destination)
+        end
+      else
+        seed.add_attribute(destination, source)
+      end
     end
+  end
+
+  def to_s
+    "Map from #{source} to #{destination} with #{range_mappings}"
   end
 
   def self.parse(map_header)
@@ -57,25 +86,41 @@ class Map
   end
 
   class RangeMapping
-    attr_reader :source_start, :destination_start, :range_length
+    attr_reader :source_range, :destination_range
 
     def initialize(source_start:, destination_start:, range_length:)
-      @source_start = source_start
-      @destination_start = destination_start
-      @range_length = range_length
+      @source_range = Range.new(source_start, source_start + range_length - 1)
+      @destination_range = Range.new(destination_start, destination_start + range_length - 1)
     end
 
-    def has_source?(source)
-      source_start <= source && source < source_start + range_length
+    def includes_source?(source)
+      source_range.overlaps?(source)
     end
 
     def destination(source)
-      destination_start + (source - source_start)
+      intersection = source_intersection(source)
+      start = destination_range.first + intersection.first - source_range.first
+      finish = [start + (intersection.last - intersection.first), destination_range.last].min
+      Range.new(start, finish)
+    end
+
+    def unmapped_destinations(source)
+      unmapped_destinations = []
+      return unmapped_destinations if source_range.cover?(source)
+
+      unmapped_destinations << Range.new(source.first, source_range.first - 1) if source.first < source_range.first
+      unmapped_destinations << Range.new(source_range.last + 1, source.last) if source.last > source_range.last
+
+      unmapped_destinations
+    end
+
+    def source_intersection(source)
+      Range.new([source.first, source_range.first].max, [source.last, source_range.last].min)
     end
   end
 end
 
-seeds = Seed.parse_seeds(input.shift)
+seeds = SeedRange.parse_seeds(input.shift)
 
 until input.empty?
   input.shift
@@ -85,4 +130,4 @@ until input.empty?
 end
 
 puts "Seeds: \n#{seeds.map(&:to_s).join("\n")}"
-puts "Answer: #{seeds.map(&:location).min}"
+puts "Answer: #{seeds.map(&:min_location).min}"
